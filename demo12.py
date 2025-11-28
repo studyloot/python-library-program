f91ec01e9df190676d6de2ef0a8d8d407c354f0d
Madhav-7080 <mr.madhavmaurya7080@gmail.com>
2025-11-28 11:06:03 +0530

    m

--- demo12.py
+++ demo12.py
@@ -5,8 +5,17 @@ data={
     "B":[5,15,25],
 }
 df=pd.DataFrame(data)
-df["A"]=df["A"].apply(lambda x:x*2)
+'''df["A"]=df["A"].apply(lambda x:x*2)
 print(df["A"])
 
 df["B"]=df["B"].apply(lambda x:x+3)
-print(df["B"])
+print(df["B"])'''
+
+'''result=df.apply(sum,axis=0) #axis=0 for column wise operation
+print(result)'''
+
+
+
+#for column wise operation
+colsum=df.apply(lambda x:x["A"]+x["B"],axis=1)
+print(colsum)
